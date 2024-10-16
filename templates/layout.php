68372f4d2c7370e45a4fef60ff28ab89d1de4ec6
Gipnokotik <shootinputin007@gmail.com>
2024-10-17 01:05:30 +0500

    Initial commit

---
+++ templates/layout.php
@@ -0,0 +1,13 @@
+//основной шаблон
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список абитуриентов</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
